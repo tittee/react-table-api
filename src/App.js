874b664e10207b3c4a5bc14c24f300530baa21f9
Bluenarysun <tee.emilond@gmail.com>
2021-02-05 19:12:18 +0700

    not implement loading.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.scss';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+
+import TableShow from './components/TableShow';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="App">
+          <TableShow />
+        </div>
+      </PersistGate>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/actions/types.js
@@ -0,0 +1,5 @@
+export const CREATE_LIST = 'CREATE_LIST';
+export const FETCH_LISTS = 'FETCH_LISTS';
+export const FETCH_LIST = 'FETCH_LIST';
+export const DELETE_LIST = 'DELETE_LIST';
+export const EDIT_LIST = 'EDIT_LIST';
